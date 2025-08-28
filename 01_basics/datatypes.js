e67fd0f0df06b25bc06cd35340ff36f495b478b2
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2025-08-28 14:26:49 +0000

    datatype conversion

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -14,7 +14,34 @@ number of data types:
 8) object
 */
 
-console.log(typeof "hello") //tells about types of error
-console.log(typeof null); // type: object
-console.log(typeof undefined); //type:undefined
+// console.log(typeof "hello") //tells about types of error
+// console.log(typeof null); // type: object
+// console.log(typeof undefined); //type:undefined
+
+//datatype conversion
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// console.log(typeof score);
+// console.log(score);
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+
+// string like "33" gets converted to number => 33
+//but string like "33abc" doesnt get converted and gives output NaN
+
+// let booleanloggedin = 1             //1 => true
+// let booleanloggedin = 0             //0 => false
+// let booleanloggedin = ""            //"" => false
+// let booleanloggedin = "soumya"      //"soumya" => true
+let isbooleanloggedin = Boolean(booleanloggedin)
+console.log(isbooleanloggedin);
+console.log(typeof isbooleanloggedin);
+
+
+
 
