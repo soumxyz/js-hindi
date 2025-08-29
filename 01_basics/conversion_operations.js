8b4faeaee4c5547add0338774c44213a22570ba5
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2025-08-29 16:23:13 +0000

    operator conversion

---
+++ 01_basics/conversion_operations.js
@@ -0,0 +1,23 @@
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+// let str1 = "hello"
+// let str2 = " soumya"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log(1+"2")
+// console.log(1+2);
+// console.log("1"+2+2);
+
+console.log(true);
+console.log(+true);
+console.log();
+
+
+
+
+
+
+
