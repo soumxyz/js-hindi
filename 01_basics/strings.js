36d552e8659bfa2a8164baf80523e1da24a54794
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2025-11-23 15:44:16 +0000

    string part updated and learned

---
+++ 01_basics/strings.js
@@ -0,0 +1,26 @@
+const myytname = "quackcodes"
+const repocount = 56
+
+// console.log(myytname + repocount + "value");
+
+// console.log(`hello my name is ${myytname} and my repo count is${repocount}`);
+
+// const gamename = new String('soumyahc')
+
+// console.log(gamename[3]);
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.charAt(3));
+// console.log(gamename.toUpperCase());
+// console.log(gamename.indexOf('y'));
+// console.log(gamename.indexOf('y'));
+// console.log(gamename.substring(0,4));
+// console.log(gamename.slice(-3,3));
+
+// const url = "www.youtube.com/%10home"
+// console.log(url.replace('%10' , 'next'))
+// console.log(url.trim('%10'))
+// console.log(url.includes('home'));
+
+const chanlname = "quack-codes"
+console.log(chanlname.split('-'));
