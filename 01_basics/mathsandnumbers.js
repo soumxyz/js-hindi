d5d3cd4c31b18cd1aeedd402769cca5c594590a9
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2025-11-24 02:08:19 +0000

    maths and numbers in js. operations and other functions

---
+++ 01_basics/mathsandnumbers.js
@@ -0,0 +1,39 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(1000)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+// console.log(balance.toPrecision());
+
+// const othernum = 223.743
+// console.log(othernum.toPrecision(3));
+// console.log(othernum.toPrecision(45));
+
+// const othernums = 22343.743
+// console.log(othernums.toPrecision(3));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++
+
+console.log(Math);
+// console.log(Math.abs(-10));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.sqrt(4));
+// console.log(Math.pow(4,2));
+// console.log(Math.min(4,2,5,2,7,4));
+// console.log(Math.max(4,24,2,5,2,7,4));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()*10+1);
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
