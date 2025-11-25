f9151df852a1147c0aff666faf76e47efd2a080e
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2025-11-25 18:28:59 +0000

    arrays part 2 overview

---
+++ 02_basics/arrays_pt2.js
@@ -0,0 +1,13 @@
+const marvheros = ["thor","ironman","spiderman"]
+const dcheros = ["flash","batman","superman"]
+marvheros.push(dcheros)
+console.log(marvheros);
+console.log(marvheros[3][1]);
+
+marvheros.concat(dcheros)
+console.log(marvheros);
+
+const another_array = [1,2,3,[3,4,5],4,5,[,987,98]]
+const new_arr = another_array.flat(Infinity)
+console.log(new_arr);
+console.log();
