3705ed5590223133a663e83fce1f93b00e56a6a1
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2025-11-27 04:38:50 +0000

    last topics on objects

---
+++ 02_basics/obj_destrcn.js
@@ -0,0 +1,21 @@
+// const course = {
+//     coursenam : "js in hindi",
+//     price : 999,
+//     courseinstructor : "soumya"
+// }
+
+// const{courseinstructor:instructor}=course
+// console.log(courseinstructor);
+// console.log(instructor);
+
+
+//API
+{
+    name:"soumya",
+    course:"y"
+}
+//also like this
+[
+    name:"soumya",
+    course:"y"
+]
