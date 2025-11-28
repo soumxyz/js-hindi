75357447d333b5d82a7e57847cdc4c81ebb46f4b
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2025-11-28 03:42:58 +0000

    some more updated info abt functions

--- 03_basics/funcnandparam.js
+++ 03_basics/funcnandparam.js
@@ -26,15 +26,38 @@
 //     return number1 + number2
 // }
 
-function userloginmsg(name = "sam"){
-    if(!name){
-        console.log("please enter a username");
-        return
-    }
-    return `hello ${name} you have secured a packaed of 69 LPA`
-}
+// function userloginmsg(name = "sam"){
+//     if(!name){
+//         console.log("please enter a username");
+//         return
+//     }
+//     return `hello ${name} you have secured a packaed of 69 LPA`
+// }
 
-userloginmsg("soumya") // this wont give any output as we have told only to return the value no tprint the value
-console.log(userloginmsg("soumya"));
-console.log(userloginmsg(""));
-console.log(userloginmsg());
+// userloginmsg("soumya") // this wont give any output as we have told only to return the value no tprint the value
+// console.log(userloginmsg("soumya"));
+// console.log(userloginmsg(""));
+// console.log(userloginmsg());
+
+
+// function calculatecartprice(num1){
+//     return num1
+// }
+// console.log(calculatecartprice(2));
+
+
+// function calculatetotalcartprice(...num1){
+//     return num1
+// }
+// console.log(calculatetotalcartprice(200,300,400));
+
+
+const user = {
+    name: "soumya",
+    price: 200
+} 
+
+function handleobj(anyobject) {
+    console.log(`usrname is ${anyobject.name} and price is ${anyobject.price}`);
+}
+handleobj(user);
