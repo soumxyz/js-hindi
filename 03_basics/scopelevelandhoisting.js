3c3edad08af9e83553922d13d876ce138fd9e7a7
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2025-11-30 14:45:54 +0000

    scope and hoisting basics concept

---
+++ 03_basics/scopelevelandhoisting.js
@@ -0,0 +1,35 @@
+function one(){
+    const username = "soumya"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+
+}
+one()
+
+if(true){
+    const username = "soumya"
+    if(username ==="soumya"){
+        const website = "hitesh"
+        console.log(username + website);
+        
+    }
+}
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~INTERESTING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+addone(5)
+function addone (num) {
+    return num+1
+}
+
+
+const addtwo = function(num){
+    return num+2
+}
+addtwo(4)
