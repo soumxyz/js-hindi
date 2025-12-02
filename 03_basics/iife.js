327f7ef48b9d1773fbe2ca04b53f2edef9d81942
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2025-12-02 13:57:30 +0000

    immediately invoked function

---
+++ 03_basics/iife.js
@@ -0,0 +1,15 @@
+// (function chai(){
+//     console.log('DB CONNECTED');
+// })();//the ; is to stop the iife 
+// chai()
+
+
+// (function chai(){
+//     console.log('DB CONNECTED');
+// })()  //produces error if the prev same code isnt stopped
+
+
+((name) => {
+    console.log(`hello ${name}`);
+    
+})("soumya")
