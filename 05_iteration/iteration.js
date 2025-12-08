5669c6c84925eb6c29e99ea67081b8ec9009dc16
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2025-12-08 03:19:20 +0000

    map iterations

---
+++ 05_iteration/iteration.js
@@ -0,0 +1,20 @@
+// for of loop
+
+// const arr = [1,2,3,4,5]
+
+// for (const element of arr) {
+//     console.log(element);
+    
+// }
+
+//maps
+
+const map = new Map()
+map.set("IN","INDIA")
+map.set("US","United States of America")
+map.set("UK","United Kingdom")
+map.set("AU","Austrulia")
+
+console.log(map);
+
+// 
