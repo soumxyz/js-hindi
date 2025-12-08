889d3239de5a4d13e74a3815544d248b13c21d67
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2025-12-08 05:33:50 +0000

    higher order array loop

---
+++ 05_iteration/HOAL.js
@@ -0,0 +1,21 @@
+// const myObject = {
+//     js: "javascript",
+//     cpp:"c++",
+//     rb : "ruby",
+//     swift: "swift by apple"
+// }
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is ${myObject[key]}`);
+// }
+// const programming = ["js" , "rb" , "swift" , "C++"]
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+    
+// }
+const programming = ["js" , "rb" , "swift" , "C++"]
+
+programming.forEach((item) =>{
+    console.log(item);
+    
+})
