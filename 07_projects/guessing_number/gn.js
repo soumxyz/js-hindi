4182fe22d7f53d208e969f5c03082a4f9dee605e
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2026-02-17 01:51:57 +0530

    Add guessing game and clock; update BMI form
    Add a new Number Guessing Game (gn.html, gn.css, gn.js) and a Local Time page (proj3.html, proj3.js). Implement client-side BMI calculation and validation in proj2.js (improved form submit handler, input checks, BMI computation and messaging).

---
+++ 07_projects/guessing_number/gn.js
@@ -0,0 +1,3 @@
+const num = parseInt(Math.random()*100)+1;
+
+const submitbtn = document.querySelector('#subt')
--- 07_projects/proj2.js
+++ 07_projects/proj2.js
@@ -1,1 +1,28 @@
 const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+
+    if(height === '' || height <0 || isNaN(height)){
+        results.innerHTML = `please give a  valid height ${height}`
+    }
+    else if(weight === '' || weight <0 || isNaN(weight)){
+        results.innerHTML = `please give a  valid weight ${weight}`
+    }
+    else{
+        const bmi = (weight / (height*height/10000)).toFixed(2);
+        results.innerHTML = `your BMI is ${bmi}`;
+        if(bmi < 18.5){
+            results.innerHTML = "you are underweight"
+        }
+        else if(bmi >= 18.5 && bmi < 25){
+            results.innerHTML = "you are normal weight"
+        }
+        else{
+            results.innerHTML = "you are overweight"
+        }
+    }
+});
