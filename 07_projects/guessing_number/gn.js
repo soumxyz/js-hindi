8963a0e071ec8e22d61f375cdad613a6b2a906c3
Soumya sagar nayak <nayaksoumya072005@gmail.com>
2026-02-17 22:27:44 +0530

    Add DOM queries for guess input and slot
    Add two DOM element references in gn.js: userinput (selecting #guessField) and guesslot (selecting #subt). These provide access to the user's guess input and a guess-related element for the guessing-number game; note that guesslot currently uses the same selector as the submit button.

--- 07_projects/guessing_number/gn.js
+++ 07_projects/guessing_number/gn.js
@@ -1,3 +1,5 @@
 const num = parseInt(Math.random()*100)+1;
 
 const submitbtn = document.querySelector('#subt')
+const userinput = document.querySelector('#guessField')
+const guesslot = document.querySelector('#subt')
